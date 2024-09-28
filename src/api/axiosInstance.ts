f7f0b660222b2bc8faa438f7d35ccc60c0a6ce3f
sincera <spzspz@live.com>
2024-09-28 13:11:26 +0900

    TEST : 응답 인터셉트 코드 추가

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -8,6 +8,28 @@ const instance = axios.create({
   });
 
 
+  // 응답 인터셉터
+instance.interceptors.response.use(
+  res => {
+    console.log('📍 response -> ', res);
+    return res;
+  },
+  async err => {
+    if (axios.isAxiosError(err)) {
+      const status = err.response?.status;
+
+      // 임시 코드
+      if (status === 400) {
+        console.log('데이터가 존재하지 않아요!');
+      }
+      if (status === 404) {
+        console.log('잘못된 값을 넣었어요!');
+      }
+    }
+    return Promise.reject(err);
+  },
+)
+
 export default instance;
 
   
