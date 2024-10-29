9e89acaff67e7c9d17021c53e6b4f79ce1703577
sincera <spzspz@live.com>
2024-10-29 22:58:12 +0900

    STYLE : 스타일 코드 추가

--- src/components/elements/main/Fragment/bottomNav.tsx
+++ src/components/elements/main/Fragment/bottomNav.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 
 import { BottomNavigation, BottomNavigationAction } from '@material-ui/core';
-
 import { Home, Search, AccountCircle } from '@material-ui/icons';
-
+import '../style.css'
 const BottomNav = () => {
     return (
-     <BottomNavigation className="BottomNav">
-        <BottomNavigationAction label="Home" icon={<Home />} />
-        <BottomNavigationAction label="Search" icon={<Search />} />
-        <BottomNavigationAction label="Profile" icon={<AccountCircle />} />
-      </BottomNavigation>
+      <BottomNavigation className="BottomNav">
+          <BottomNavigationAction label="Home" icon={<Home />} />
+          <BottomNavigationAction label="Search" icon={<Search />} />
+          <BottomNavigationAction label="Profile" icon={<AccountCircle />} />
+        </BottomNavigation>
       );
 }
 
