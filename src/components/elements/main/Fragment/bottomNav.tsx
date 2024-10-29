0100efc1bbe91e61934bc59ac226a6e709301a2c
sincera <spzspz@live.com>
2024-10-29 11:16:22 +0900

    FEAT : BOTTOM 화면 구성

---
+++ src/components/elements/main/Fragment/bottomNav.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { BottomNavigation, BottomNavigationAction } from '@material-ui/core';
+
+import { Home, Search, AccountCircle } from '@material-ui/icons';
+
+const BottomNav = () => {
+    return (
+     <BottomNavigation className="BottomNav">
+        <BottomNavigationAction label="Home" icon={<Home />} />
+        <BottomNavigationAction label="Search" icon={<Search />} />
+        <BottomNavigationAction label="Profile" icon={<AccountCircle />} />
+      </BottomNavigation>
+      );
+}
+
+export default BottomNav;
